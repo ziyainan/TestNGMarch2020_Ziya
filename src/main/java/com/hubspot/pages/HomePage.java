b7e3b0bbd95b640d4c73e1bf294d374fc8be83a3
ziyainan <47232215+ziyainan@users.noreply.github.com>
2020-03-13 20:43:38 -0400

    TestNGMarch_2020hatirlaticinot

---
+++ src/main/java/com/hubspot/pages/HomePage.java
@@ -0,0 +1,62 @@
+package com.hubspot.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.hubspot.base.BasePage;
+import com.hubspot.util.Constants;
+import com.hubspot.util.ElementUtil;
+
+public class HomePage extends BasePage{
+	
+	WebDriver driver;
+	ElementUtil elementUtil;
+	
+	
+	By header = By.xpath("//h1[@class='private-page__title']");
+	By accountName = By.xpath("//span[@class='account-name ']");
+	
+	// BURAYA IKI FARKLI LOCATOR KOYARAK HOME --> CONTACT ILISKISI KURACAGIZ
+	By contactMainTab=By.id("nav-primary-contacts-branch");
+	By contactChildTab=By.id("nav-secondary-contacts");
+	
+	//Constructor
+	public HomePage(WebDriver driver){
+		this.driver = driver;
+		elementUtil = new ElementUtil(driver);
+	}
+	public String getHomePageTitle(){
+		return elementUtil.waitForGetPageTitle(Constants.HOME_PAGE_TITLE);
+		
+		//return driver.getTitle();
+	}
+
+public String getHomePageHeader(){
+	return elementUtil.doGetText(header);
+	
+	//return driver.findElement(header).getText();
+	
+}
+
+public String verifyLoggedInAccountName(){
+	
+	
+	return elementUtil.doGetText(accountName);
+	//return driver.findElement(accountName).getText();
+}
+
+	//helper metod
+private void ClickOnContacts(){ // private helper method, User dan data gizleriz, sadece sonuc gosterecegiz
+	// data yi private sakla , public ile cagiririz
+	elementUtil.doClick(contactMainTab);
+	elementUtil.doClick(contactChildTab);
+}
+
+public ContactsPage gotoContactsPage(){ // SSSS sorunu hallettim
+	ClickOnContacts();
+	return new ContactsPage(driver);
+}
+	
+	
+
+}
